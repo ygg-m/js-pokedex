8ce22ba2981052898cc4dccc632cb7c821195bfc
Ygor Goulart <ygorgm95@gmail.com>
2022-10-26 09:29:32 -0300

    refactor and added comments

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,39 +1,55 @@
+// ui elements
 const pokemonList = document.getElementById("pokemon-list");
 const BtnLoadMore = document.getElementById("btn-load-more");
+
+// api variables
+const maxCardLoad = 15;
 const limit = 10;
 let offset = 0;
 
+// button, load more cards
 BtnLoadMore.addEventListener("click", () => {
   offset += limit;
-  loadPokeCards(offset, limit);
+  const nextPageCardCount = offset + limit;
+
+  if (nextPageCardCount >= maxCardLoad) {
+    const newLimit = maxCardLoad - offset;
+    loadPokeCards(offset, newLimit);
+
+    BtnLoadMore.parentElement.removeChild(BtnLoadMore);
+  } else {
+    loadPokeCards(offset, limit);
+  }
 });
 
-function pokeCard(pokemon) {
-  const { name, order, types, id, mainType } = pokemon;
-  return `
-    <div class="pokemon ${mainType}">
-        <p class="number">#${order}</p>
-        <p class="name">${name}</p>
-        <div class="types">
-          ${types.map((type) => `<p class=${type}>${type}</p>`).join("")}
-        </div>
-        <div class="image">
-          <img
-            src=https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png
-            alt="${name}"
-          />
-        </div>
-    </div>
-        `;
-}
-
+// load pokemon cards
 function loadPokeCards(offset, limit) {
   useApi
     .getPokemons(offset, limit)
     .then((res = []) => {
-      pokemonList.innerHTML += res.map(pokeCard).join("");
+      pokemonList.innerHTML += res
+        .map((pokemon) => {
+          const { name, types, id, mainType } = pokemon;
+          return `
+          <div class="pokemon ${mainType}">
+              <p class="number">#${id}</p>
+              <p class="name">${name}</p>
+              <div class="types">
+                ${types.map((type) => `<p class=${type}>${type}</p>`).join("")}
+              </div>
+              <div class="image">
+                <img
+                  src=https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png
+                  alt="${name}"
+                />
+              </div>
+          </div>
+              `;
+        })
+        .join("");
     })
     .catch((err) => console.log(err));
 }
 
+// first load
 loadPokeCards(offset, limit);
