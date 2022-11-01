a8446545aee6fd8a5e1ab29fbd757ed68f719f74
Ygor Goulart <ygorgm95@gmail.com>
2022-11-01 13:29:54 -0300

    refactor

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -36,54 +36,39 @@ function loadPokeCards(offset, limit) {
 // first load
 loadPokeCards(offset, limit);
 
-//
+// draw poke cards at main page
 function drawPokeCard(pokemon) {
-  const { name, types, id, mainType } = pokemon;
-  const imgURL =
-    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/";
+  const { name, imgURL, types, id, mainType } = pokemon;
+  const pokeNumber = "#" + lpad(id, 3, 0);
 
   // create ui
   const pokeCard = document.createElement("article");
-  const info = document.createElement("div");
-  const number = document.createElement("p");
-  const pokeName = document.createElement("p");
-  const typesContainer = document.createElement("div");
-  const type = document.createElement("p");
-  const imgContainer = document.createElement("div");
-  const img = document.createElement("img");
 
   // add classes
   pokeCard.classList.add("pokemon", mainType);
-  info.classList.add("info");
-  number.classList.add("number");
-  pokeName.classList.add("name");
-  typesContainer.classList.add("types");
-  type.classList.add("type");
-  imgContainer.classList.add("image");
-
-  // add content
-  number.innerText = "#" + lpad(id, 3, 0);
-  pokeName.innerText = name;
-  img.src = imgURL + id + ".png";
-  img.alt = name;
 
   // draw the card
   pokemonList.append(pokeCard);
-  pokeCard.append(info, imgContainer);
-  info.append(number, pokeName, typesContainer);
-  imgContainer.append(img);
+  pokeCard.innerHTML = `
+      <div class="info">
+        <p class="number">${pokeNumber}</p>
+        <p class="name">${name}</p>
+        <div class="types">
+          ${types.map((type) => `<p class=${type}>${type}</p>`).join("")}
+        </div>
+      </div>
+      <div class="image">
+        <img
+          src=${imgURL}
+          alt=${name}
+        />
+      </div>`;
 
   // add event listener
   pokeCard.addEventListener("click", () => {
     drawModalWithPokemon(pokemon);
-
     toggleModal();
   });
-
-  // add types
-  typesContainer.innerHTML = `${types
-    .map((type) => `<p class=${type}>${type}</p>`)
-    .join("")}`;
 }
 
 // toggle modal on/off
@@ -96,48 +81,186 @@ function resetModal() {
   modal.innerHTML = "";
 }
 
-// draw modal
+// draw modal on click
 function drawModalWithPokemon(pokemon) {
-  const { name, types, id, mainType } = pokemon;
+  const { name, imgURL, types, id, mainType } = pokemon;
+  const pokeNumber = lpad(id, 3, 0);
 
-  document.body.style = "overflow: no-scroll";
-
-  // reset modal
+  // clean modal HTML
   resetModal();
 
-  // ui elements
-  // header
-  const header = document.createElement("div");
-  const number = document.createElement("h3");
-  const pokeName = document.createElement("h2");
+  // close button
   const closeButton = document.createElement("img");
-
-  // showcase
-  const showcase = document.createElement("div");
-  
-
-  // add classes
-  header.classList.add("header");
-  number.classList.add("number");
-  pokeName.classList.add("name");
-
-  // add data
   closeButton.id = "close-button";
   closeButton.src = "./assets/img/close.svg";
-
-  // add content
-  number.innerHTML = "<span>#</span>" + lpad(id, 3, 0);
-  pokeName.innerText = name;
-
-  // draw ui
-  modal.append(header);
-  header.append(number, pokeName, closeButton);
 
   // close modal button
   closeButton.addEventListener("click", () => {
     toggleModal();
-    document.body.style = "";
+    modal.classList.remove(mainType);
   });
+
+  // header
+  const header = document.createElement("div");
+  const number = document.createElement("h3");
+  const pokeName = document.createElement("h2");
+  header.classList.add("header");
+  number.innerHTML = `<span>#</span>${pokeNumber}</h3>`;
+  pokeName.innerHTML = `<h2 class="name">${name}</h2>`;
+  header.append(number, pokeName, closeButton);
+
+  // main content
+  const mainContent = document.createElement("div");
+
+  // modal classes
+  modal.classList.add(mainType);
+
+  // draw ui
+  modal.append(header);
+  modal.append(mainContent);
+  mainContent.innerHTML += `
+
+  <div class="showcase">
+  <img
+          src=${imgURL}
+          alt=${name}
+        />
+      </div>
+      <div class="content">
+        <div class="main-info-container">
+          <div class="row types">
+            <p class="title">Type</p>
+            <ul class="list">
+              ${types.map((type) => `<p class=${type}>${type}</p>`).join("")}
+            </ul>
+          </div>
+          <div class="row abilities">
+            <p class="title">Abilities</p>
+            <ul class="list">
+              <li>item</li>
+              <li>item</li>
+            </ul>
+          </div>
+          <div class="row catch-rate">
+            <p class="title">Catch rate</p>
+            <p class="rate">
+              00%
+              <span class="explanation">
+                When a normal Poké Ball is thrown at full health in Generation
+                VII.
+              </span>
+            </p>
+          </div>
+
+          <div class="row weight-height">
+            <div class="flex">
+              <p class="title">Weight</p>
+              <p class="content">0.0 <span>kg</span></p>
+            </div>
+            <div class="flex">
+              <p class="title">Height</p>
+              <p class="content">0.0 <span>cm</span></p>
+            </div>
+          </div>
+        </div>
+        <div class="stats-container">
+          <p class="title">Stats</p>
+          <div class="content">
+            <div class="stat hp">
+              <p class="title">HP</p>
+              <p class="content">45</p>
+            </div>
+
+            <div class="stat atk">
+              <p class="title">Atk</p>
+              <p class="content">45</p>
+            </div>
+
+            <div class="stat def">
+              <p class="title">Def</p>
+              <p class="content">45</p>
+            </div>
+
+            <div class="stat atk">
+              <p class="title">S.Atk</p>
+              <p class="content">45</p>
+            </div>
+
+            <div class="stat def">
+              <p class="title">S.Def</p>
+              <p class="content">45</p>
+            </div>
+
+            <div class="stat spd">
+              <p class="title">Spd</p>
+              <p class="content">45</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="evolutions-container">
+          <p class="title">Evolutions</p>
+          <div class="evolution-path">
+            <div class="evolution grass">
+              <div class="img-container">
+                <img
+                  src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
+                  alt="Bulbasaur"
+                />
+              </div>
+              <p class="name">Bulbasaur</p>
+              <div class="types">
+                <p class="type grass">Grass</p>
+                <p class="type poison">Poison</p>
+              </div>
+            </div>
+
+            <div class="path">
+              <div class="img-container">
+                <img src="./assets/img/rare-candy.png" alt="Rare Candy" />
+              </div>
+              <p class="path-name">Level <span>7</span></p>
+              <object data="./assets/img/arrow.svg" type=""></object>
+            </div>
+
+            <div class="evolution grass">
+              <div class="img-container">
+                <img
+                  src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png"
+                  alt="Ivysaur"
+                />
+              </div>
+              <p class="name">Ivysaur</p>
+              <div class="types">
+                <p class="type grass">Grass</p>
+                <p class="type poison">Poison</p>
+              </div>
+            </div>
+
+            <div class="path">
+              <div class="img-container">
+                <img src="./assets/img/rare-candy.png" alt="Rare Candy" />
+              </div>
+              <p class="path-name">Level <span>7</span></p>
+              <object data="./assets/img/arrow.svg" type=""></object>
+            </div>
+
+            <div class="evolution grass">
+              <div class="img-container">
+                <img
+                  src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png"
+                  alt="Venusaur"
+                />
+              </div>
+              <p class="name">Venusaur</p>
+              <div class="types">
+                <p class="type grass">Grass</p>
+                <p class="type poison">Poison</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>`;
 }
 
 // returns the string with 0 before it
