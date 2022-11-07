768181c71918dc400da5a6b5f8a700906e3b0901
Ygor Goulart <ygorgm95@gmail.com>
2022-11-06 21:58:56 -0300

    evolution chain working as intended

--- assets/scripts/drawUI.js
+++ assets/scripts/drawUI.js
@@ -11,58 +11,91 @@ const drawModal = {
 
   types(types) {
     return `
-<div class="row types">
-    <p class="title">Type</p>
-    <ul class="list">
-        ${types.map((type) => `<p class=${type}>${type}</p>`).join("")} 
-    </ul>
-</div>`;
+      <div class="row types">
+          <p class="title">Type</p>
+          <ul class="list">
+              ${types.map((type) => `<p class=${type}>${type}</p>`).join("")} 
+          </ul>
+      </div>`;
   },
 
   abilities(abilities) {
     return `
-<div class="row abilities">
-    <p class="title">Abilities</p>
-    <ul class="list">
-        ${abilities
-          .map((e) => `<p class="ability">${e.ability.name}</p>`)
-          .join("")}
-    </ul>
-</div>`;
+      <div class="row abilities">
+          <p class="title">Abilities</p>
+          <ul class="list">
+              ${abilities
+                .map((e) => `<p class="ability">${e.ability.name}</p>`)
+                .join("")}
+          </ul>
+      </div>`;
   },
 
   weightHeight(weight, height) {
     return `
-<div class="row weight-height">
-    <div class="flex">
-        <p class="title">Weight</p>
-        <p class="content">${weight / 10} <span>kg</span></p>
-    </div>
-    <div class="flex">
-        <p class="title">Height</p>
-        <p class="content">${height / 10} <span>m</span></p>
-    </div>
-</div>`;
+      <div class="row weight-height">
+          <div class="flex">
+              <p class="title">Weight</p>
+              <p class="content">${weight / 10} <span>kg</span></p>
+          </div>
+          <div class="flex">
+              <p class="title">Height</p>
+              <p class="content">${height / 10} <span>m</span></p>
+          </div>
+      </div>`;
   },
 
   stats(stats) {
     return `
-<div class="stats-container">
-  <p class="title">Stats</p>
-  <div class="content">
-    ${stats
-      .map((stat) => {
-        const value = stat.base_stat;
-        const statName = renameStat(stat.stat.name);
-        const className = renameClass(stat.stat.name);
-        return `
-      <div class="stat ${className}">
-        <p class="title">${statName}</p>
-        <p class="content">${value}</p>
+      <div class="stats-container">
+        <p class="title">Stats</p>
+        <div class="content">
+          ${stats
+            .map((stat) => {
+              const value = stat.base_stat;
+              const statName = renameStat(stat.stat.name);
+              const className = renameClass(stat.stat.name);
+              return `
+            <div class="stat ${className}">
+              <p class="title">${statName}</p>
+              <p class="content">${value}</p>
+            </div>
+            `;
+            })
+            .join("")}
+      </div>`;
+  },
+
+  evolution: {
+    path(trigger) {
+      return `
+      <div class="path">
+        <div class="img-container">
+          <img src="./assets/img/rare-candy.png" alt="Rare Candy" />
+        </div>
+        <p class="path-name">Level <span>7</span></p>
+        <object data="./assets/img/arrow.svg" type=""></object>
       </div>
       `;
-      })
-      .join("")}
-</div>`;
+    },
+
+    card(poke) {
+      const { id, name, types } = poke;
+      return `
+        <div class="evolution ${types[0].type.name}">
+          <div class="img-container">
+            <img
+              src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png"
+              alt=${name}
+            />
+          </div>
+            <p class="name">${name}</p>
+          <div class="types">
+          ${types
+            .map((e) => `<p class='type ' + ${e.type.name}>${e.type.name}</p>`)
+            .join("")} 
+          </div>
+        </div>`;
+    },
   },
 };
